ab9482a7696a13797c2e9350e60452aca79e45a6
Corey Atkinson <catkinson2025@gmail.com>
2023-08-30 09:28:30 -0500

    Initial Commit

---
+++ getInfoNotes/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  getInfoNotes
+//
+//  Created by COREY ATKINSON on 8/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ageOutlet: UITextField!
+    @IBOutlet weak var nameOutlet: UITextField!
+    @IBOutlet weak var helloOutlet: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitAction(_ sender: UIButton) {
+      //get name out of outlet
+        var name = nameOutlet.text!
+        var age = Int(ageOutlet.text!)
+        if let a = age{
+           age = a + 10
+            helloOutlet.text = "hello \(name)! You will be \(age) years old in 10 years."
+        }
+        else{
+            helloOutlet.text = "invalad data"
+        }
+      
+        
+    }
+    
+    
+}
+
